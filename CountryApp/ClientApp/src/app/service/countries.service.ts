ead1520221d0914ab7cf5a1970e0b98d7c16bce0
KikatoIV <39209669+KikatoIV@users.noreply.github.com>
2024-04-02 14:08:18 +0200

    File changes and clean up for prod

--- CountryApp/ClientApp/src/app/service/countries.service.ts
+++ CountryApp/ClientApp/src/app/service/countries.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../../Models/country.model';
+import { environment } from '../../environments/environment'; //
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class CountryService {
-  private apiUrl = 'https://localhost:7093/api/countries';
+
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
-
+  
   getAllCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>(this.apiUrl);
+    if(this.apiUrl === '')
+    {
+      this.apiUrl =  window.location.href;
+    }
+
+    return this.http.get<Country[]>(`${this.apiUrl}/countries`);
   }
 }
